973a94cb3d5eddbfd32e89657b39b70b9d311095
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-02 09:43:18 -0500

    refactor: remove ESLint disable comments and add logging in main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
   await app.listen(envs.port);
+
+  logger.log(`Client Gateway is running on port ${envs.port}`);
+  logger.log(`Client Gateway is running on http://localhost:${envs.port}`);
 }
 bootstrap();
