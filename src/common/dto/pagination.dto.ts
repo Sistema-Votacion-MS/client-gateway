6ca426673c0d10e555b72b4f76d0d2163dd7a241
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 11:34:42 -0500

    feat: add ApiProperty decorators to PaginationDto for Swagger documentation

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive } from "class-validator";
 
@@ -5,10 +6,20 @@ export class PaginationDto {
   @IsPositive()
   @IsOptional()
   @Type(() => Number)
+  @ApiProperty({
+    description: 'The page number for pagination',
+    example: 1,
+    required: false,
+  })
   page?: number = 1;
 
   @IsPositive()
   @IsOptional()
   @Type(() => Number)
+  @ApiProperty({
+    description: 'The number of items per page for pagination',
+    example: 10,
+    required: false,
+  })
   limit?: number = 10;
 }
