d0d29c0e6c73822dc7055706e1e82f723a04af6f
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 11:02:09 -0500

    feat: add Election module with controller, service, and DTOs for election management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { NatsModule } from './transports/nats.module';
+import { ElectionModule } from './election/election.module';
 
 @Module({
-  imports: [/* UsersModule,  */AuthModule, NatsModule],
+  imports: [/* UsersModule,  */AuthModule, NatsModule, ElectionModule],
   controllers: [],
   providers: [],
 })
